aca1e6cf096ee791670f25dcdb7f3246131644bb
Anastasia <zharkovaanasta@yandex.ru>
2023-10-27 11:38:56 +0300

    fix: CodeStyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/director/DirectorDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/director/DirectorDbStorage.java
@@ -7,7 +7,6 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.model.Director;
 
